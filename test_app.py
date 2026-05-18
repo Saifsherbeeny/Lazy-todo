ad70c334df742fdbe0133b0aa809a6a62960cd4c
Saifsherbeeny <saifsherbeeny1@gmail.com>
2026-05-18 21:04:22 +0200

    sec10

--- test_app.py
+++ test_app.py
@@ -1,15 +1,20 @@
 import pytest
 from app import app as flask_app
+from app import db 
 
 @pytest.fixture
 def app():
-    yield flask_app
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    flask_app.config['TESTING'] = True
+    
+    with flask_app.app_context():
+        db.create_all() 
+        yield flask_app
 
 @pytest.fixture
 def client(app):
     return app.test_client()
 
 def test_homepage(client):
-    """Test that the index/login page loads successfully."""
     response = client.get('/')
     assert response.status_code == 200
